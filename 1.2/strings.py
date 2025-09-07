59dceff426730897d6d7b8ec932527d0c65a7b4b
ajur26 <andrzej.w.jur@gmail.com>
2025-09-07 21:24:00 +0200

    update

---
+++ 1.2/strings.py
@@ -0,0 +1,19 @@
+name = "jan kowalski"
+print(name)
+print(name.title())
+print(name.upper())
+print(name.lower())
+first_name = "jan"
+last_name = "kowalski"
+full_name = first_name + " " + last_name
+print(full_name)
+print(full_name.title())
+print("Python")
+print("\tPython")
+print("\nPython")
+print("Jezyki: \nPython\nC\nJavaScript")
+print("Jezyki: \n\tPython\n\tC\n\tJavaScript")
+favorite_language = "python "
+print(favorite_language)
+favorite_language.rstrip()
+print(favorite_language)
