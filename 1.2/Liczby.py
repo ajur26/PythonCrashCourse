54ddd2113f11ed230117e3085b146a38d5cee939
ajur26 <andrzej.w.jur@gmail.com>
2025-10-05 19:30:11 +0200

    update

---
+++ 1.2/Liczby.py
@@ -0,0 +1,10 @@
+dodawanie = 2+3
+mnozenie = 2*3
+potega = 3**2
+dzialanie = 2+3*4
+print(dodawanie,mnozenie,potega,dzialanie)
+zmiennoprzecinkowe = 0.1 + 0.1
+zmiennoprzecinkowe1 = 2 * 0.1
+zmiennoprzecinkowe2 = 0.2 + 0.1
+print(zmiennoprzecinkowe,zmiennoprzecinkowe1,zmiennoprzecinkowe2)
+
