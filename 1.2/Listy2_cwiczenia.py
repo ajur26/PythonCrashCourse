cc28e641fbff5c7d868685107886251df4c158e9
ajur26 <andrzej.w.jur@gmail.com>
2025-10-10 07:28:33 +0200

    update

--- 1.2/Listy2_cwiczenia.py
+++ 1.2/Listy2_cwiczenia.py
@@ -1,2 +1,14 @@
-Guests = ["Adrian Kowalski", "Mateusz Adamkowski", "Tomasz Narozny"]
-Invitation = "Witaj" + Guests[0] + ""
+guests = ["Adrian Kowalski", "Mateusz Adamkowski", "Tomasz Narozny"]
+invitation = ", witaj - zapraszam Cię na obiad"
+print(guests[0] + invitation)
+print(guests[1] + invitation)
+print(guests[2] + invitation)
+nieprzyjdzie = guests.pop(1)
+print('nie pojawi sie: ' + nieprzyjdzie)
+guests.append('Maciej Donkiel')
+print(guests)
+print(guests[0] + invitation)
+print(guests[1] + invitation)
+print(guests[2] + invitation)
+print("Znalazlem wiekszy stol, zmiesci sie wiecej osob.")
+guests.insert()
